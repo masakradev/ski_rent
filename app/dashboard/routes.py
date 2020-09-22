c9d55c0ae52c6ef4232e7d16bd5c5eae887b79a4
Mateusz Kaleta <masakramr@gmail.com>
2020-09-22 15:07:37 +0200

    Dodawanie wypozyczen, oddawanie, listy wypozyczen routing + menu.

--- app/dashboard/routes.py
+++ app/dashboard/routes.py
@@ -4,3 +4,20 @@ from app.dashboard import bp
 @bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@bp.route('/dodaj_wypozyczenie')
+def dodaj_wypozyczenie():
+    return render_template('dodaj_wypozyczenie.html')
+
+@bp.route('/oddanie_wypozyczenia')
+def oddanie_wypozyczenia():
+    return render_template('oddanie_wypozyczenia.html')
+
+@bp.route('/wypozyczenia_lista_dzisiaj')
+def wypozyczenia_lista_dzisiaj():
+    return render_template('wypozyczenia_lista_dzisiaj.html')
+
+@bp.route('/wypozyczenia_lista_wszystkie')
+def wypozyczenia_lista_wszystkie():
+    return render_template('wypozyczenia_lista_wszystkie.html')
